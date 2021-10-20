d7265a85676f6a839c4e014464b76981f7600940
alex-bcht <alex.bauchet95160@gmail.com>
2021-10-20 15:33:04 +0200

    menu

---
+++ app/src/main/java/com/kamiath/projetdevmobile/ForfaitActivity.kt
@@ -0,0 +1,12 @@
+package com.kamiath.projetdevmobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ForfaitActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forfait)
+    }
+}
--- app/src/main/java/com/kamiath/projetdevmobile/MainActivity.kt
+++ app/src/main/java/com/kamiath/projetdevmobile/MainActivity.kt
@@ -4,6 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import android.content.Intent
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import androidx.core.content.ContextCompat.getSystemService
+
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,12 +26,20 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
+        when (item.itemId) {
+            R.id.forfait_mobile -> {
+                val forfaitActivityIntent = Intent(this@MainActivity, ForfaitActivity::class.java)
+                startActivity(forfaitActivityIntent)
+            }
+            R.id.box_internet -> Toast.makeText(this, "@string/box_internet", Toast.LENGTH_SHORT)
+            R.id.promos -> Toast.makeText(this, "@string/promos", Toast.LENGTH_SHORT)
+            R.id.international -> Toast.makeText(this, "@string/international", Toast.LENGTH_SHORT)
+            R.id.operateurs -> Toast.makeText(this, "@string/operateurs", Toast.LENGTH_SHORT)
+        }
+        return super.onOptionsItemSelected(item)
+        /*return when (item.itemId) {
             R.id.forfait_mobile -> true
             else -> super.onOptionsItemSelected(item)
-        }
+        }*/
     }
 }
